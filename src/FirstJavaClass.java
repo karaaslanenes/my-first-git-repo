29ef386d2d2d6d1eb0d5a3b8d8f753b700a40faf
Enes Karaaslan <fromsmyrna237@gmail.com>
2022-03-26 15:57:19 -0400

    this the updated one

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,5 +1,7 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
-        System.out.println("learning git");
+        System.out.println("learning github");
+        System.out.println("adding a new code ");
+        System.out.println("this is another update");
     }
 }
